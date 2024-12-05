857e18ac64512e627b246bafeb101829ff2f90d4
Andrew Budniak <andrey_bduanyak@mail.ru>
2024-12-05 18:34:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// class Person {
+
+//     #age;
+    
+//     constructor(age){
+//         this.#age=age;
+//     }
+
+//     get age(){
+//         return this.#age
+//     }
+    
+//     set age(value){
+//         if (value >= 0){
+//             this.#age=value;
+//         } else {
+//             console.log("Error")
+//         }
+//     }
+// }
+
+// const p1 = new Person(23);
+
+
+// console.log(p1.age)
+// p1.age = 55;
+// console.log(p1.age)
+// p1.age = -10;
+// console.log(p1.age)
